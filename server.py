000da33e73c1d6d7f66ada0eb9d2a2bc216caf41
Aldar0809 <aldar_san2009@mail.ru>
2024-03-18 16:24:10 +0300

    yeah1

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify
 import logging
-import json
 # импортируем функции из нашего второго файла geo
-from geo import get_country, get_distance, get_coordinates
 
 app = Flask(__name__)
 
@@ -23,40 +21,31 @@ def main():
             'end_session': False
         }
     }
-    handle_dialog(response, request.json)
+    handler(response, request.json)
     logging.info('Request: %r', response)
     return jsonify(response)
 
 
-def handle_dialog(res, req):
-    user_id = req['session']['user_id']
-    if req['session']['new']:
-        res['response']['text'] = \
-            'Привет! Я могу показать город или сказать расстояние между городами!'
-        return
-    # Получаем города из нашего
-    cities = get_cities(req)
-    if not cities:
-        res['response']['text'] = 'Ты не написал название ни одного города!'
-    elif len(cities) == 1:
-        res['response']['text'] = 'Этот город в стране - ' + \
-                                  get_country(cities[0])
-    elif len(cities) == 2:
-        distance = get_distance(get_coordinates(
-            cities[0]), get_coordinates(cities[1]))
-        res['response']['text'] = 'Расстояние между этими городами: ' + \
-                                  str(round(distance)) + ' км.'
-    else:
-        res['response']['text'] = 'Слишком много городов!'
-
-
-def get_cities(req):
-    cities = []
-    for entity in req['request']['nlu']['entities']:
-        if entity['type'] == 'YANDEX.GEO':
-            if 'city' in entity['value']:
-                cities.append(entity['value']['city'])
-    return cities
+def handler(event, context):
+    """
+    Точка входа для облачной функции.
+    :param event: содержимое request.json().
+    :param context: информация о текущем контексте выполнения.
+    :return: ответ будет представлен в виде json автоматически.
+    """
+    text = 'Привет, я повторю все, что вы скажете'
+    if 'request' in event and \
+            'original_utterance' in event['request'] \
+            and len(event['request']['original_utterance']) > 0:
+        text = event['request']['original_utterance']
+    return {
+        'version': event['version'],
+        'session': event['session'],
+        'response': {
+            'text': text,
+            'end_session': 'false'
+        },
+    }
 
 
 if __name__ == '__main__':
